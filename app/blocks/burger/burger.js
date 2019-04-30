7fa716e8c815587a0962165c4cbb9ade61a9ae8f
Maya Soloveva <maya.soloveva2015@ya.ru>
2019-04-30 17:02:05 +0300

    Верхнее меню

--- app/blocks/burger/burger.js
+++ app/blocks/burger/burger.js
@@ -2,20 +2,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	function $$(selector, context) {
 		context = context || document;
-		let elements = context.querySelectorAll(selector);
+		const elements = context.querySelectorAll(selector);
 		return Array.prototype.slice.call(elements);
 	}
 
 	function showBurgerTarget() {
-		let targetId = this.getAttribute('data-target-id');
-		let targetClassToggle = this.getAttribute('data-target-class-toggle');
+		const targetId = this.getAttribute('data-target-id');
+		const targetClassToggle = this.getAttribute('data-target-class-toggle');
+		const ariaHidden = this.getAttribute('aria-expanded');
 		if (targetId && targetClassToggle) {
+			if (ariaHidden === 'true') {
+				this.setAttribute('aria-expanded', 'false');
+			}
+			else if (ariaHidden === 'false') {
+				this.setAttribute('aria-expanded', 'true');
+			}
 			this.classList.toggle('burger--close');
 			document.getElementById(targetId).classList.toggle(targetClassToggle);
 		}
 	}
 
-	let burgers = $$('.burger');
+	const burgers = $$('.burger');
 
 	for (let i = 0; i < burgers.length; i++) {
 		let burger = burgers[i];
